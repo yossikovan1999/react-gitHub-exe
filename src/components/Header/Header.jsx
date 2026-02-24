c975499e2cf0ba40580bdd91c46a8986c3c0c2de
Yossi Kovan <yossikovan@gmail.com>
2026-02-24 11:54:34 +0200

    adding html structure

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,25 @@
+import "./Header.css"
+
+function Header() {
+  return (
+    <header>
+      <div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 20 20"
+        >
+          <path
+            fill="#e9e9e9"
+            d="M17.604 3.332C12.99 4 12.075 2.833 10 1C7.925 2.833 7.01 4 2.396 3.332C-.063 15.58 10 19 10 19s10.063-3.42 7.604-15.668m-5.131 9.977L10 12.009l-2.472 1.3L8 10.556l-2-1.95l2.764-.401L10 5.7l1.236 2.505L14 8.606l-2 1.949z"
+          />
+        </svg>
+      </div>
+      <h1>Bomb detection application</h1>
+      <div>Simulation Active</div>
+    </header>
+  );
+}
+
+export default Header;
