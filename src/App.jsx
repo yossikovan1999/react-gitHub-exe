36d1a90c36fb6a35f44b5c39529ae7d7240a63f9
Yossi Kovan <yossikovan@gmail.com>
2026-02-24 15:29:28 +0200

    adding the board header

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import { useState } from 'react'
 import './App.css'
 import Header from './components/Header/Header'
 import CardSection from './components/CardsSection/CardsSection'
-import GameProvider from './context/gameProvider'
+import GameProvider from './context/gameProvider';
+import BoardHeader from './components/BoardHeader/BoardHeader';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,6 +14,7 @@ function App() {
       <Header/>
       <GameProvider>
         <CardSection/>
+        <BoardHeader/>
       </GameProvider>
     </>
   )
---
+++ src/components/BoardHeader/BoardHeader.jsx
@@ -0,0 +1,13 @@
+import "./BoardHeader.css"
+
+
+function BoardHeader(){
+
+    return(
+        <section className="board-header">
+            Locate and neutralize all bombs before time runs out.
+        </section>
+    )
+}
+
+export default BoardHeader;
