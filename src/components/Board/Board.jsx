ab12aac7e5c4948a0903ebcf20ada78b3cf19d82
Yosef Kovan <154745088+YosefKovan@users.noreply.github.com>
2026-02-26 10:46:31 +0200

    adding changes

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,12 +1,21 @@
 import "./Board.css";
 import { useState } from "react";
+import {useGameProvider} from "../../context/gameProvider";
 
 function Board(){
     
-    const [chosen, setChosen] = useState([]);
+    const {board, setBoard} = useGameProvider();
+    
 
-    function handleClick(row, col){        
-        setChosen((prev)=>[...prev, `${row}-${col}`])
+    function handleClick(row, col){
+        
+        if(board[row][col].revealed){
+            return;
+        }else{
+            const newBoard = board.map((item)=>{
+                item.row === row && item.col === col ? 
+            })
+        }
     }
 
     function createBoard(){
@@ -15,11 +24,14 @@ function Board(){
         
         for(let row = 0; row < 10; row++){
             for(let col = 0; col < 10; col++){
-                if(chosen.includes(`${row}-${col}`)){
-                    boardItems.push(<div className="chosen-cell"></div>)
+                
+                if(board[row][col].bomb && board[row][col].revealed){
+                    boardItems.push(<div key={`${row}-${col}`} className="bomb"></div>);
+                }else if(!board[row][col].bomb && board[row][col].revealed){
+                    boardItems.push(<div key={`${row}-${col}`} className="revealed-cell"></div>)
                 }else{
-                    boardItems.push(<div onClick={()=>handleClick(row, col)} className="board-cell"></div>)
-                } 
+                    boardItems.push(<div key={`${row}-${col}`} className="board-cell" onClick={()=>handleClick(row, col)}></div>)
+                }
             }
         }
 
--- src/context/gameProvider.jsx
+++ src/context/gameProvider.jsx
@@ -3,28 +3,61 @@ import useTimer from "../hooks/useTimer";
 
 const GameContex = createContext();
 
-export function useGameProvider(){
-    return useContext(GameContex);
+export function useGameProvider() {
+  return useContext(GameContex);
 }
 
+//=======================================
+//          helper functions
+//=======================================
+function initializeBombs(grid, amountOfBombs, rows = 10, cols = 10) {
+  let counter = 0;
 
-function GameProvider({children}){
- 
-    const [bombs, setBombs] = useState(5);
-    const [board, setBoard] = useState({rows : 10, cols : 8});
-    const {humanTime, timeLeft} = useTimer(3);
+  while (counter < amountOfBombs) {
+    const row = Math.floor(Math.random() * rows);
+    const col = Math.floor(Math.random() * cols);
 
-       
-    const values = {
-        bombs, setBombs, humanTime, timeLeft, board
+    if (grid[row][col].bomb !== true) {
+      grid[row][col].bomb = true;
+      counter++;
+    }
+  }
+}
+
+function initializeBoard(rows = 10, cols = 10) {
+  const grid = [];
+
+  for (let row = 0; row < rows; row++) {
+    const gridRow = [];
+    for (let col = 0; col < cols; col++) {
+      gridRow.push({ row: row, col: col, bomb: false, revealed: false });
     }
 
-    return(
-        <GameContex.Provider value={values}>
-            {children}
-        </GameContex.Provider>
-    )
+    grid.push(gridRow);
+  }
 
+  initializeBombs(grid, 5, 10, 10);
+
+  return grid;
+}
+
+//=======================================
+//            Game Provider
+//=======================================
+function GameProvider({ children }) {
+  const [bombsCount, setBombsCount] = useState(5);
+  const [board, setBoard] = useState(()=>initializeBoard());
+  const { humanTime, timeLeft } = useTimer(3);
+
+  const values = {
+    bombsCount,
+    setBombsCount,
+    humanTime,
+    timeLeft,
+    board,
+  };
+
+  return <GameContex.Provider value={values}>{children}</GameContex.Provider>;
 }
 
 export default GameProvider;
